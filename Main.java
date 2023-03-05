d42c387905b51000507ef90dd0d497037435835a
JoseAngelMorales <105880422+JoseAngelMorales@users.noreply.github.com>
2023-03-05 17:23:19 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,13 +3,27 @@ package HDT6;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        
         Scanner teclado = new Scanner(System.in);
         Factory factory = new Factory();
-        Mapa<String, String> mapa = new Mapa<>();
+        Mapa gmapa = new Mapa();
+
+        String menu = "Bienvenido al programa, elija la opcion de implementacion\n1. Hashmap\n2. Linked Hashmap\n3. Treemap";
+        System.out.println(menu);
+        int tipomapa = teclado.nextInt();
+        teclado.nextLine();
+        while(tipomapa>=1 && tipomapa<=3){
+            System.out.println(menu);
+            tipomapa = teclado.nextInt();
+            teclado.nextLine();
+        }
+        
+        Map<String, String> mapa = gmapa.newMap(tipomapa);
 
         System.out.println("Ingrese la direccion del archivo: ");
         String archivo = teclado.nextLine();
@@ -19,7 +33,7 @@ public class Main {
             String linea = reader.readLine();
             while(linea != null){
                 Namer namer = factory.getNamer(linea);
-                mapa.agregar(namer.getNombre(), namer.getTipoproducto());
+                mapa.put(namer.getNombre(), namer.getTipoproducto());
                 linea = reader.readLine();
             }
 
